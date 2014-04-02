898f438cff8bb93f789df433e84410e6fd815572
tcarrill <thomas.carrill@gmail.com>
2014-04-02 14:25:05 -0400

    Setup nav for continue menu

--- src/tetris/states/ContinueMenu.java
+++ src/tetris/states/ContinueMenu.java
@@ -16,13 +16,7 @@ public class ContinueMenu extends BaseMenu implements KeyListener {
     private static final String PASSKEY_STRING = Tetris.resources.getString("passkey");
     protected int passkeyTextCenterWidth;
     private StateBasedGame game;
-
-    private static final String[][] KEYPAD = new String[][]  {
-            {"7", "8", "9"},
-            {"4", "5", "6"},
-            {"1", "2", "3"},
-            {"-2", "0", "-2"}
-    };
+    private Integer[] passkey = new Integer[10];
 
     @Override
     public int getID() {
@@ -33,6 +27,9 @@ public class ContinueMenu extends BaseMenu implements KeyListener {
     public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
         game = stateBasedGame;
         passkeyTextCenterWidth = menuFont.getWidth(PASSKEY_STRING) / 2;
+        for (int i = 0; i < passkey.length; i++) {
+            passkey[i] = null;
+        }
     }
 
     @Override
@@ -42,17 +39,10 @@ public class ContinueMenu extends BaseMenu implements KeyListener {
         menuFont.drawString(70, 100, BACK_STRING, selectedItem == BACK ? Color.red : Color.white);
         menuFont.drawString(Tetris.CENTER_WIDTH - passkeyTextCenterWidth, 120, PASSKEY_STRING, Color.white);
 
-        int x = 344;
-        int y = 200;
-        for (String[] row : KEYPAD) {
-            for (String key : row) {
-                if (!key.equals("-2")) {
-                    menuFont.drawString(x, y, key,  selectedItem == Integer.parseInt(key) ? Color.red : Color.white);
-                }
-                x += 50;
-            }
-            x = 344;
-            y += 50;
+        int startX = 260;
+        for (int i = 0; i < 10; i++) {
+            menuFont.drawString(startX, 200, passkey[i] == null ? "_" : passkey[i].toString(), selectedItem == i ? Color.red : Color.white);
+            startX += (i == 2 || i == 5) ? 50 : 25;
         }
 
         menuFont.drawString(Tetris.CENTER_WIDTH - legal1TextCenterWidth, 500, LEGAL1, Color.white);
@@ -72,40 +62,37 @@ public class ContinueMenu extends BaseMenu implements KeyListener {
                 }
                 break;
             case Input.KEY_UP:
-                if (selectedItem == 7 || selectedItem == 8 || selectedItem == 9) {
-                    selectedItem = BACK;
-                } else if (selectedItem == 0) {
-                    selectedItem = 2;
-                } else if (selectedItem == BACK) {
-
-                } else {
-                    selectedItem += 3;
+                if (selectedItem != BACK) {
+                    passkey[selectedItem] = passkey[selectedItem] == null ? 0 : passkey[selectedItem] + 1;
+                    if (passkey[selectedItem] > 9) {
+                        passkey[selectedItem] = 0;
+                    }
                 }
                 break;
             case Input.KEY_DOWN:
                 if (selectedItem == BACK) {
-                    selectedItem = 5;
-                } else if (selectedItem == 1 || selectedItem == 2) {
                     selectedItem = 0;
-                } else if (selectedItem == 0) {
-                    selectedItem = 8;
                 } else {
-                    selectedItem -= 3;
+                    passkey[selectedItem] = passkey[selectedItem] == null ? 9 : passkey[selectedItem] - 1;
+                    if (passkey[selectedItem] < 0) {
+                        passkey[selectedItem] = 9;
+                    }
                 }
                 break;
             case Input.KEY_LEFT:
-                if (selectedItem == 8 || selectedItem == 5 || selectedItem == 2 ||
-                    selectedItem == 9 || selectedItem == 6 || selectedItem == 3) {
-                    selectedItem -= 1 ;
+                if (selectedItem != BACK) {
+                    selectedItem--;
                 }
                 break;
             case Input.KEY_RIGHT:
-                if (selectedItem == 7 || selectedItem == 4 || selectedItem == 1 ||
-                        selectedItem == 8 || selectedItem == 5 || selectedItem == 2) {
-                    selectedItem += 1 ;
+                if (selectedItem != BACK) {
+                    selectedItem++;
+                }
+
+                if (selectedItem == 10) {
+                    selectedItem = BACK;
                 }
                 break;
-
         }
     }
 }
