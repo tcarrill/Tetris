bca5650099570711389b7038e98190a8dcdd2853
tcarrill <thomas.carrill@gmail.com>
2014-04-03 16:30:42 -0400

    Basic passkey activation logic, could be done better

--- src/tetris/states/ContinueMenu.java
+++ src/tetris/states/ContinueMenu.java
@@ -4,6 +4,9 @@ import org.newdawn.slick.*;
 import org.newdawn.slick.state.StateBasedGame;
 import tetris.Tetris;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by thomas on 3/30/14.
  */
@@ -12,11 +15,15 @@ public class ContinueMenu extends BaseMenu implements KeyListener {
 
     private static final int BACK = -1;
     private int selectedItem = BACK;
+
     private static final String BACK_STRING = Tetris.resources.getString("back");
     private static final String PASSKEY_STRING = Tetris.resources.getString("passkey");
+
     protected int passkeyTextCenterWidth;
-    private StateBasedGame game;
-    private Integer[] passkey = new Integer[10];
+    private Tetris game;
+    private Integer[] passkey;
+    private Map<String, Integer> passkeys;
+    private StringBuilder sb;
 
     @Override
     public int getID() {
@@ -25,11 +32,24 @@ public class ContinueMenu extends BaseMenu implements KeyListener {
 
     @Override
     public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
-        game = stateBasedGame;
+        game = (Tetris)stateBasedGame;
         passkeyTextCenterWidth = menuFont.getWidth(PASSKEY_STRING) / 2;
-        for (int i = 0; i < passkey.length; i++) {
-            passkey[i] = null;
+    }
+
+    @Override
+    public void enter(GameContainer container, StateBasedGame game) throws SlickException {
+        super.enter(container, game);
+        if (sb == null) {
+            sb = new StringBuilder();
         }
+
+        if (passkeys == null) {
+            passkeys = new HashMap<String, Integer>();
+            passkeys.put("0073735963", 21);
+        }
+
+        passkey = new Integer[10];
+        sb.setLength(0);
     }
 
     @Override
@@ -39,10 +59,10 @@ public class ContinueMenu extends BaseMenu implements KeyListener {
         menuFont.drawString(70, 100, BACK_STRING, selectedItem == BACK ? Color.red : Color.white);
         menuFont.drawString(Tetris.CENTER_WIDTH - passkeyTextCenterWidth, 120, PASSKEY_STRING, Color.white);
 
-        int startX = 260;
+        int startX = 285;
         for (int i = 0; i < 10; i++) {
             menuFont.drawString(startX, 200, passkey[i] == null ? "_" : passkey[i].toString(), selectedItem == i ? Color.red : Color.white);
-            startX += (i == 2 || i == 5) ? 50 : 25;
+            startX += (i == 2 || i == 5) ? 40 : 20;
         }
 
         menuFont.drawString(Tetris.CENTER_WIDTH - legal1TextCenterWidth, 500, LEGAL1, Color.white);
@@ -58,6 +78,20 @@ public class ContinueMenu extends BaseMenu implements KeyListener {
         switch(key) {
             case Input.KEY_ENTER:
                 if (selectedItem == BACK) {
+                    for (Integer integer : passkey) {
+                        if (integer == null) {
+                            break;
+                        }
+
+                        sb.append(integer);
+                    }
+                    if (sb.length() == 10) {
+                        Integer level = passkeys.get(sb.toString());
+                        if (level != null) {
+                            System.out.println("PASS KEY ACTIVATED - LEVEL: "+level);
+                        }
+                    }
+
                     game.enterState(MainMenu.ID);
                 }
                 break;
