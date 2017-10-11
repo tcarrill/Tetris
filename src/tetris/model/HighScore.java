7723ae7b9d97ea6c1ad7737d2396645ca56689f1
tcarrill <thomas.carrill@gmail.com>
2017-10-11 18:13:31 -0400

    Added HighScore model

---
+++ src/tetris/model/HighScore.java
@@ -0,0 +1,22 @@
+package tetris.model;
+
+public class HighScore {
+    private String initials;
+    private int score;
+
+    public String getInitials() {
+        return initials;
+    }
+
+    public void setInitials(String initials) {
+        this.initials = initials;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
